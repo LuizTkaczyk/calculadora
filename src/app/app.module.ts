6bb600c4f954305eb5bd33341db9ea939e5b5a91
Luiz Antonio <luiz8907@hotmail.com>
2021-11-02 11:05:08 -0300

    caculadoraAngular

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CalculadoraModule} from './calculadora/calculadora.module'
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, 
+    CalculadoraModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
